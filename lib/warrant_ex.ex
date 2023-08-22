2cf6eb1b12d46d6ea85d89441e69fdf90dd4ab52
KhemPoudel <me.khempoudel@gmail.com>
2023-08-22 15:34:18 +0545

    project init

---
+++ lib/warrant_ex.ex
@@ -0,0 +1,18 @@
+defmodule WarrantEx do
+  @moduledoc """
+  Documentation for `WarrantEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WarrantEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/warrant_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule WarrantExTest do
+  use ExUnit.Case
+  doctest WarrantEx
+
+  test "greets the world" do
+    assert WarrantEx.hello() == :world
+  end
+end
